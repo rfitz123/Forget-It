c5ca73c8a8a9ce437d1b0ce715ad227c85a01106
riley fitzpatrick <rileyfitz123@gmail.com>
2019-10-19 03:29:56 -0400

    defined Tree class

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,22 @@
+from flask import render_template
 from app import app
 
-@app.route(
+
+class Tree:
+    """
+    Tree class is a node that holds a reference to a node 
+    on its left and right while holding a value itself.
+    """
+
+    def __init__(self, cargo, left=None, right=None, name=None):
+        self.cargo = cargo
+        self.left  = left
+        self.right = right
+        self.name = name
+
+    def __str__(self):
+        return str(self.cargo)
+
+@app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
